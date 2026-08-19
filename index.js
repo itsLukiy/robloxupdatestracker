03453c9c53788c390caaca20119a100581f420e0
itsLukiy <josuesolessanchez01@gmail.com>
2026-08-19 00:26:32 -0500

    Update index.js

--- index.js
+++ index.js
@@ -53,3 +53,15 @@ async function checkLiveVersion() {
 }
 
 client.login(process.env.DISCORD_TOKEN);
+
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 4000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
